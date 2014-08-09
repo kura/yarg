9b308c4cd6b5a9df7ac468acb5efdf13f0865de0
Kura <kura@kura.io>
2014-08-09 11:50:22 +0100

    Exception tests

--- tests/test_exceptions.py
+++ tests/test_exceptions.py
@@ -3,14 +3,29 @@ import unittest
 from yarg import HTTPError
 
 
-class TestHTTPError(unittest.TestCase):
+class TestHTTPErrorWithReason(unittest.TestCase):
 
     def setUp(self):
         self.error = HTTPError(status_code=300,
                                reason="Test")
 
     def test_repr(self):
-        self.assertEqual('300 Test', self.error.__repr__())
+        self.assertEqual('<HTTPError 300 Test>',
+                         self.error.__repr__())
 
     def test_str(self):
-        self.assertEqual('300 Test', self.error.__str__())
+        self.assertEqual('<HTTPError 300 Test>',
+                         self.error.__str__())
+
+class TestHTTPErrorNoReason(unittest.TestCase):
+
+    def setUp(self):
+        self.error = HTTPError()
+
+    def test_repr(self):
+        self.assertEqual('<HTTPError>',
+                         self.error.__repr__())
+
+    def test_str(self):
+        self.assertEqual('<HTTPError>',
+                         self.error.__str__())
